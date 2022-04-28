2808f5513bc344c3243c7c475ca272c44c3c5ccb
Daynis Olman <daynis@ditconsulting.com.au>
2022-04-28 18:18:39 +1000

    Created PNG image email signature generation capability

---
+++ src/generate_png_email_signature.php
@@ -0,0 +1,84 @@
+<?php
+
+//Set the Content Type
+header('Content-type: image/png');
+
+// Create Image From Existing File
+$png_image = imagecreatefrompng('../images/baseline_image.png');
+
+// Allocate A Color For The Text
+$black = imagecolorallocate($png_image, 0, 0, 0);
+
+// Set Path to Font File
+$font_path = '../fonts/Helvetica.ttf';
+
+// Set Text to Be Printed On Image
+$fullname = $_POST["firstName-input"] . " " . $_POST["lastName-input"];
+$firstname = $_POST["firstName-input"];
+$jobtitle = $_POST["jobTitle-input"];
+$website = "www.naturalwayofliving.com";
+$email = $_POST["emailAddress-input"] . "@naturalwayofliving.com";
+$mobilephone = $_POST["mobilePhone-input"];
+
+//Pre-process data before printing to image
+if (strlen(trim($_POST['officePhone-input'])) > 0)  {
+    $officephone = $_POST["officePhone-input"];
+    $phonenumbers = $mobilephone . " | " .  $officephone;
+}else{
+    $phonenumbers = $mobilephone;
+}
+if (strlen(trim($_POST['department-input'])) > 0) {
+    $department = $_POST["department-input"]." | ";
+    $organisation = "$department" ."Natural Way Of Living";
+}else{
+    $organisation = "Natural Way Of Living";
+}
+
+// Print Name on Image
+imagettftext($png_image, 15, 0, 202, 40, $black, $font_path, $fullname);
+
+// Print JobTitle on Image
+imagettftext($png_image, 11, 0, 202, 62, $black, $font_path, $jobtitle);
+
+// Print Organisation + Department name on Image
+imagettftext($png_image, 11, 0, 202, 84, $black, $font_path, $organisation);
+
+// Print Office phone + Mobile phone on Image
+imagettftext($png_image, 10, 0, 227, 153, $black, $font_path, $phonenumbers);
+
+// Print Office phone + Mobile phone on Image
+imagettftext($png_image, 10, 0, 227, 175, $black, $font_path, $email);
+
+// Print Company Website on Image
+imagettftext($png_image, 10, 0, 227, 199, $black, $font_path, $website);
+
+// Save image in temporary directory
+$signatureFilename = "nwl-email-signature.png";
+imagepng($png_image, "../tmp/" . $signatureFilename);
+
+#imagepng($png_image);
+#die();
+
+// Save generated image email signature as .ZIP and Download
+$zip = new ZipArchive;
+$tmp_file = "../tmp/email-signature-png.zip";
+if ($zip->open($tmp_file,  ZipArchive::CREATE)) {
+    $zip->addFile("../tmp/".$signatureFilename, $signatureFilename);
+    $zip->close();
+    echo 'Archive created!';
+    header("Content-disposition: attachment; filename=$firstname.email-signature.zip");
+    header('Content-type: application/zip');
+    ob_clean();
+    flush();
+    readfile($tmp_file);
+} else {
+    echo 'Failed!';
+}
+
+// Send Image to Browser
+#imagejpeg($jpg_image);
+
+// Clear Memory
+#imagedestroy($jpg_image);
+?>
+
